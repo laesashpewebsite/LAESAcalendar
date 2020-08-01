37237e6d245cb99ba9d000c009f1bb7db1512d2c
TopSM <ostavo.p@gmail.com>
2020-07-31 20:11:14 -0400

    Updates

---
+++ precache-manifest.bb1f3eb7481786b1141581596c481f2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e466b33a705cec229b22373d42278093",
+    "url": "/.github.io/LAESAcalendar/index.html"
+  },
+  {
+    "revision": "d4a0d467beb8c2972704",
+    "url": "/.github.io/LAESAcalendar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6952b58b599eb8644ff3",
+    "url": "/.github.io/LAESAcalendar/static/js/2.4692e87a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/LAESAcalendar/static/js/2.4692e87a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a0d467beb8c2972704",
+    "url": "/.github.io/LAESAcalendar/static/js/main.81bf560b.chunk.js"
+  },
+  {
+    "revision": "94305e61fd9e33cd769b",
+    "url": "/.github.io/LAESAcalendar/static/js/runtime-main.22de0dc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/.github.io/LAESAcalendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
