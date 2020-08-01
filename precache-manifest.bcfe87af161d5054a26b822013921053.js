fb0922022d41490825a8cf950d88059a3dd14289
TopSM <ostavo.p@gmail.com>
2020-07-31 21:22:01 -0400

    Updates

---
+++ precache-manifest.bcfe87af161d5054a26b822013921053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394f36506af48327cc366cb85a422dc0",
+    "url": "/LAESAcalendar/index.html"
+  },
+  {
+    "revision": "ca87b0397506446fb990",
+    "url": "/LAESAcalendar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad0e6ca40e817d1742dd",
+    "url": "/LAESAcalendar/static/js/2.d6578d60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LAESAcalendar/static/js/2.d6578d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca87b0397506446fb990",
+    "url": "/LAESAcalendar/static/js/main.8c78d5c6.chunk.js"
+  },
+  {
+    "revision": "58528a85a09e09cafcbb",
+    "url": "/LAESAcalendar/static/js/runtime-main.8785343a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LAESAcalendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/.github.io/LAESAcalendar/precache-manifest.574a6b361d1ff315ebba873759d81f88.js"
+  "/LAESAcalendar/precache-manifest.bcfe87af161d5054a26b822013921053.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.github.io/LAESAcalendar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LAESAcalendar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
